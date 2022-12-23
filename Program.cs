5a309c55aea2f6c1389c53930aa89c70e5adc15c
mohammedhossam16 <121051466+mohammedhossam16@users.noreply.github.com>
2022-12-23 14:26:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2_Perfect_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start_num, end_num;
+
+            Console.Write("Enter the start number of range : ");
+            start_num = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the end number of range : ");
+            end_num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+            Console.WriteLine("perfect numbers in given range are: ");
+
+            for (int i = start_num; i <= end_num; i++)
+            {
+                int sum = 0;
+
+                for (int t = 1; t < i; t++)
+                {
+                    if (i % t == 0)
+                    {
+                        sum = sum + t;
+                    }
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(". \t" + i);
+
+                }
+            }
+        }
+    }
+}
